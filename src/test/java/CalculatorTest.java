b63dc75d9266b7fc931792efd93880eeeb9e96d5
Ax / Yuya Ono <axyojp@axgws.com>
2024-03-27 12:57:39 +0000

    fin 02_03

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,15 +4,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
-    Calculator calculator = new Calculator();
+  Calculator calculator = new Calculator();
 
-    @Test
-    public void testAdd() {
-        int expected = 3;
+  @Test
+  public void testAdd() {
+    int expected = 3;
 
-        int actual = calculator.add(1, 2);
+    int actual = calculator.add(1, 2);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
+  @Test
+  public void testSubtract() {
+    int expected = 3;
+
+    int actual = calculator.subtract(4, 1);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testMultiply() {
+    int expected = 6;
+
+    int actual = calculator.multiply(3, 2);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testDivide() {
+    int expected = 3;
+
+    int actual = calculator.divide(6, 2);
+
+    assertEquals(expected, actual);
+  }
 }
