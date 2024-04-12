83cf05ab7e3f9b3992e38c8ef29620d6296dd3e6
Ax / Yuya Ono <axyojp@axgws.com>
2024-04-12 12:43:19 +0000

    fin 03_04

--- src/main/java/HairSalon.java
+++ src/main/java/HairSalon.java
@@ -1,24 +1,33 @@
-import java.util.Arrays;
-
 public class HairSalon {
 
-    private String[] openingDays = {"Monday", "Tuesday"};
+    private String[] openingDays = { "Monday", "Tuesday" };
 
     // This method should return the openingDays array.
     public String[] getOpeningDays() {
-        return new String[]{};
+        return openingDays;
     }
 
-    // This method should return true if the weekday passed in is present in the openingDays array.
-    // If not it should return false. If the String passed in does not end in the letters "day",
-    // it should throw an IllegalArgumentException with a message explaining that only weekdays are valid.
+    // This method should return true if the weekday passed in is present in the
+    // openingDays array.
+    // If not it should return false. If the String passed in does not end in the
+    // letters "day",
+    // it should throw an IllegalArgumentException with a message explaining that
+    // only weekdays are valid.
     public boolean isOpen(String weekDay) {
-        return false;
+        boolean isFound = false;
+
+        if (!weekDay.endsWith("day")) {
+            throw new IllegalArgumentException("only weekdays are valid");
+        }
+
+        for (String str : this.openingDays) {
+            if (str.equals(weekDay)) {
+                isFound = true;
+                break;
+            }
+        }
+
+        return isFound;
     }
-
-
-
-
-
 
 }
--- src/test/java/HairSalonTest.java
+++ src/test/java/HairSalonTest.java
@@ -1,3 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class HairSalonTest {
@@ -6,22 +15,37 @@ class HairSalonTest {
 
     @Test
     public void testGetOpeningDays() {
+        String[] expected = new String[] { "Monday", "Tuesday" };
 
+        String[] actual = hairSalon.getOpeningDays();
+
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     public void testSalonIsOpenOnMonday() {
+        boolean expected = true;
+        boolean actual = hairSalon.isOpen("Monday");
 
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testSalonIsClosedOnWednesday() {
+        boolean expected = false;
+        boolean actual = hairSalon.isOpen("Wednesday");
 
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testExceptionIsThrownIfWeekdayDoesNotEndInDay() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> hairSalon.isOpen("hoge"));
 
+        String expected = "only weekdays are valid";
+        String actual = exception.getMessage();
+
+        assertEquals(expected, actual);
     }
 
 }
